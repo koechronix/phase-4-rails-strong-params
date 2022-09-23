50e84c20f2dc531b073e16c30688fea634907637
koechronix <koechronix@yahoo.com>
2022-09-23 16:57:05 +0300

    Completed mass params

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,14 +1,18 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
+  # 
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(params.permit(:name, :species))
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +24,10 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+  private
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
